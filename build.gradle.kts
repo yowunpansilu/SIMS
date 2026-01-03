c60323f10b8bfde29738829a2678e3733c53b582
yowunpansilu <pansilu03@gmail.com>
2026-01-03 20:50:13 +0530

    Build files Issue jdk 25&23

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,4 +19,8 @@ subprojects {
             languageVersion.set(JavaLanguageVersion.of(25))
         }
     }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.release.set(23)
+    }
 }
