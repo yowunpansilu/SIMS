2b7a3386d58fa3e87e83e7454a1a6eee17ddc26a
yowunpansilu <pansilu03@gmail.com>
2026-01-11 16:44:12 +0530

    Implement LoginView with AtlantaFX

--- client/src/main/java/com/sims/client/App.java
+++ client/src/main/java/com/sims/client/App.java
@@ -1,10 +1,10 @@
 package com.sims.client;
 
+import com.sims.client.view.LoginView;
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import atlantafx.base.theme.PrimerDark;
 
 public class App extends Application {
@@ -13,11 +13,12 @@ public class App extends Application {
     public void start(Stage stage) {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
 
-        var root = new StackPane(new Label("SIMS Client - Running"));
-        var scene = new Scene(root, 800, 600);
+        var root = new StackPane(new LoginView());
+        var scene = new Scene(root, 900, 600);
 
         stage.setTitle("SIMS - Student Information Management System");
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
