51ec3314d42e7b520516ec8e4369d82c840156ed
yowunpansilu <pansilu03@gmail.com>
2026-01-14 18:55:43 +0530

    feat: Pin Lombok to version 1.18.36 and add a new  file with Java installation paths and caching configuration.

--- server/src/main/java/com/sims/server/model/User.java
+++ server/src/main/java/com/sims/server/model/User.java
@@ -1,15 +1,9 @@
 package com.sims.server.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -27,4 +21,65 @@ public class User {
 
     private String fullName;
     private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String role, String fullName, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
